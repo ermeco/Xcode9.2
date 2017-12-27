31823a6739d18d1e54ade76436063e2200e75e1e
Ruud Meuwissen <r.meuwissen@me.com>
2017-12-27 14:37:09 +0100

    Initial Commit

---
+++ Xcode92/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode92
+//
+//  Created by Ruud Meuwissen on 27-12-17.
+//  Copyright © 2017 Ruud Meuwissen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode92/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode92
+//
+//  Created by Ruud Meuwissen on 27-12-17.
+//  Copyright © 2017 Ruud Meuwissen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
